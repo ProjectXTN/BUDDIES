14663019ca08937c626549c47bbf1ee84cc98849
ProjectXTN <pedroxtn@gmail.com>
2022-09-06 23:06:45 +0200

    Mise a jour le Dashboard
    Mise a jour les fonctionnalité du Dashboard User

--- Project_symfony/src/Form/UserType.php
+++ Project_symfony/src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,7 +24,7 @@ class UserType extends AbstractType
             ->add('Picture')
             ->add('Biography')
             ->add('isExpat')
-            ->add('created_at')
+            ->add('created_at', DateTimeType::class)
             ->add('groupe')
             ->add('Publication')
             ->add('Form')
