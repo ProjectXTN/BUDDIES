3b8f723e22dfebd9a4712e93cbd98133d8814c7c
ProjectXTN <pedroxtn@gmail.com>
2022-09-08 10:16:45 +0200

    migration to mySQL

--- Project_symfony/migrations/Version20220908081131.php
+++ Project_symfony/migrations/Version20220908081131.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220907152137 extends AbstractMigration
+final class Version20220908081131 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220907152137 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD isconnected DATE NULL');
+        $this->addSql('ALTER TABLE user CHANGE isconnected isconnected DATE NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP isconnected');
+        $this->addSql('ALTER TABLE user CHANGE isconnected isconnected DATE DEFAULT NULL');
     }
 }
