acfe4b1f362d64a5c827b8f821fb8dd7655e335f
3-dedel <adel.belarbi310@gmail.com>
2022-09-12 16:12:09 +0200

    formulaire
    Enregistrement choix user

--- Project_symfony/src/Controller/QuizController.php
+++ Project_symfony/src/Controller/QuizController.php
@@ -6,6 +6,8 @@ use App\Repository\FormRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class QuizController extends AbstractController
 {
@@ -17,16 +19,26 @@ class QuizController extends AbstractController
             'form' => $form,
             'step1' => true,
             'step2' => false,
-            'step3' => false
+            'step3' => false,
+            'step4' => false
         ]);
     }
 
-    
-
     #[Route('/quiz/step2', name: 'app_quiz_2')]
-    public function stepTwo(FormRepository $formRepository): Response
+    public function stepTwo(Request $request, FormRepository $formRepository, EntityManagerInterface $entityManagerInterface): Response
     {
-        //traitement du form1 
+        $user = $this->getUser();
+        //traitement du form1 ( Recupere les choix du step 1)
+        foreach($request->get('valueCheckbox') as $row){
+            $form = $formRepository->findOneById($row);
+            $user->addForm($form);
+        }
+
+        //persist (Previent qu'il faut enregistrer les choix)
+        $entityManagerInterface->persist($user);
+
+        //flush ( il excute l'enregistement)
+        $entityManagerInterface->flush();
 
 
         $form = $formRepository->findByStep(2);
@@ -34,20 +46,59 @@ class QuizController extends AbstractController
             'form' => $form,
             'step2' => true,
             'step1' => false,
-            'step3' => false
+            'step3' => false,
+            'step4' => false
         ]);
     }
 
     #[Route('/quiz/step3', name: 'app_quiz_3')]
-    public function stepThree(FormRepository $formRepository): Response
+    public function stepThree(Request $request ,FormRepository $formRepository, EntityManagerInterface $entityManagerInterface): Response
     {
+
+        $user = $this->getUser();
+        
+        foreach($request->get('valueCheckbox') as $row){
+            $form = $formRepository->findOneById($row);
+            $user->addForm($form);
+        }
+
+        $entityManagerInterface->persist($user);
+
+        
+        $entityManagerInterface->flush();
+
+
+
         $form = $formRepository->findByStep(3);
         return $this->render('quiz/step.html.twig', [
             'form' => $form,
             'step1' => false,
             'step2' => false,
-            'step3' => true
+            'step3' => true,
+            'step4' => false
            
         ]);
+    }
+
+    #[Route('/quiz/step4', name: 'app_quiz_4')]
+    public function stepFour(Request $request ,FormRepository $formRepository, EntityManagerInterface $entityManagerInterface): Response
+    {
+
+        $user = $this->getUser();
+        
+        foreach($request->get('valueCheckbox') as $row){
+            $form = $formRepository->findOneById($row);
+            $user->addForm($form);
+        }
+
+        $entityManagerInterface->persist($user);
+
+        
+        $entityManagerInterface->flush();
+
+
+        $form = $formRepository->findByStep(4);
+        return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+            
     }
 }
