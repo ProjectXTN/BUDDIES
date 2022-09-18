f7b3bda810179276b7c1b82094ca00b209097183
ProjectXTN <pedroxtn@gmail.com>
2022-09-18 21:14:11 +0200

    page dashboard/community
    creation page community / ajoute de funcionalites sur la page dashboard

--- Project_symfony/src/Controller/GroupController.php
+++ Project_symfony/src/Controller/GroupController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Group;
 use App\Repository\GroupRepository;
+use App\Repository\PublicationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +14,36 @@ class GroupController extends AbstractController
     #[Route('/group', name: 'app_group')]
     public function index(GroupRepository $groupRepository): Response
     {
-        return $this->render('group/index.html.twig', [
+        return $this->render('group/index2.html.twig', [
             'controller_name' => 'GroupController',
             // il faut faire un boucle pour utilise le findAll est recupere touts les group
             'group' => $groupRepository->findAll()
+        ]);
+    }
+
+
+    //   ***************  il faut faire la route por prend le ID du group ************
+
+
+    #[Route('/community/page', name: 'app_group_details', methods: ['GET'])]
+    public function details(GroupRepository $groupRepository, PublicationRepository $publicationRepository): Response
+    {
+       
+/*         $tabFriends = [];
+        foreach($this->getUser()->getFriends() as $row){
+            array_push($tabFriends, $row->getId());
+        }
+
+        $showButtonFriend = true;
+        if(in_array($user->getId(), $tabFriends)){
+            $showButtonFriend = false;
+        }  */
+        
+
+        return $this->render('group/index.html.twig', [
+            'controller_name' => 'CommunityPageController',
+            'group' => $groupRepository->findOneById(3),
+            'publication' => $publicationRepository->findOneById(9)
         ]);
     }
 }
